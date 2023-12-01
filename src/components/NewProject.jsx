683c668ec5069c99ce5895a0a0894428a6ee6b20
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-12-01 10:55:57 +0100

    Styling Buttons & Inputs with Tailwind CSS

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,15 @@ import Input from "./Input";
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button>Cancel</button>
+          <button className="text-stone-800 hover:text-red-600">Cancel</button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="px-6 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
         </li>
       </menu>
       <div>
