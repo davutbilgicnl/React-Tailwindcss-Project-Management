2235f7c3ee084b775582227237bc50be380a54c8
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-12-01 16:12:51 +0100

    Handling Project Creation & Updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectState);
 
   let content;
 
@@ -44,7 +44,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-7">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
